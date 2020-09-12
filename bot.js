f489047653147890d8ff982f037e47de94ecbe5f
Naomi Keller <thatstraveling@gmail.com>
2020-09-12 17:02:29 -0400

    better command syntax

--- bot.js
+++ bot.js
@@ -3,11 +3,20 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
+    client.user.setActivity("yeehaw"); // "Playing <>" status message for bot
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-        message.reply('pong');
+client.on("message", async message => {
+    if (message.author.client) return;
+    if (message.channel.type === "dm") return;
+
+    let prefix = config.prefix; // prefix is '!' for executing commands
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+
+    if (cmd === `${prefix}test`) {
+        return message.channel.send("hello world");
     }
 });
 
