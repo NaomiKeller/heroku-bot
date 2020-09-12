7cf3d98dd46c963c6bcabfe7d9934d824b609c8a
Naomi Keller <thatstraveling@gmail.com>
2020-09-12 17:59:48 -0400

    testing dates and auto deploy

--- bot.js
+++ bot.js
@@ -17,9 +17,14 @@ client.on("message", async message => {
     let messageArray = message.content.split(" ");
     let cmd = messageArray[0];
     let args = messageArray.slice(1);
+    let d = new Date();
 
     if (cmd === `${prefix}ping`) {
         return message.channel.send("pong");
+    }
+
+    if (cmd === `${prefix}date`) {
+        return message.channel.send(`${d}`);
     }
 });
 
