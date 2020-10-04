56bd7b268be11f42c2c6da598a4ac4d440287090
Naomi Keller <thatstraveling@gmail.com>
2020-10-04 16:56:44 -0400

    hopefully fixed

--- bot.js
+++ bot.js
@@ -39,38 +39,19 @@ client.on("message", async message => {
 });
 
 client.on("message", async message => {
-    let prefix = config.prefix;
-    
+    if (message.author.bot) return;
+    if (message.channel.type === "dm") return;
+
+    let prefix = config.prefix; 
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+    let d = new Date();
+    let helpArray = ["!ping"," !date"]; // any command we add should be added to this array
+
     if (cmd === `${prefix}difFunc`) {
         return message.channel.send(`yes`);
     }
-});
-
-client.on('raw', packet => {
-    // We don't want this to run on unrelated packets
-    if (!['MESSAGE_REACTION_ADD', 'MESSAGE_REACTION_REMOVE'].includes(packet.t)) return;
-    // Grab the channel to check the message from
-    const channel = client.channels.get(packet.d.channel_id);
-    // There's no need to emit if the message is cached, because the event will fire anyway for that
-    if (channel.messages.has(packet.d.message_id)) return;
-    // Since we have confirmed the message is not cached, let's fetch it
-    channel.fetchMessage(packet.d.message_id).then(message => {
-        // Emojis can have identifiers of name:id format, so we have to account for that case as well
-        const emoji = packet.d.emoji.id ? `${packet.d.emoji.name}:${packet.d.emoji.id}` : packet.d.emoji.name;
-        // This gives us the reaction we need to emit the event properly, in top of the message object
-        const reaction = message.reactions.get(emoji);
-        // Adds the currently reacting user to the reaction's users collection.
-        if (reaction) reaction.users.set(packet.d.user_id, client.users.get(packet.d.user_id));
-        // Check which type of event it is before emitting
-        if (packet.t === 'MESSAGE_REACTION_ADD') {
-            client.emit('messageReactionAdd', reaction, client.users.get(packet.d.user_id));
-            return message.channel.send(`You have subscribed`);
-        }
-        if (packet.t === 'MESSAGE_REACTION_REMOVE') {
-            client.emit('messageReactionRemove', reaction, client.users.get(packet.d.user_id));
-            return message.channel.send(`You have unsubscribed`);
-        }
-    });
 });
 
 // do not touch this. this is how our bot links to our code from discord. 
