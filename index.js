6c22675b002cb92328496e11a9b1fccf1bf7801c
Naomi Keller <thatstraveling@gmail.com>
2020-10-03 21:16:35 -0400

    im fucking shit up

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const { Server } = require('ws');
+
+const wss = new Server({ server });
+
+wss.on('connection', (ws) => {
+    console.log('Client connected');
+    ws.on('close', () => console.log('Client disconnected'));
+});
+
+setInterval(() => {
+    wss.clients.forEach((client) => {
+      client.send(new Date().toTimeString());
+    });
+  }, 1000);
+  
+var HOST = location.origin.replace(/^http/, 'ws')
+var ws = new WebSocket(HOST);
+var el;
+
+ws.onmessage = function (event) {
+el = document.getElementById('server-time');
+el.innerHTML = 'Server time: ' + event.data;
+};
+  
+
+// -------------------------------------------------------------------------- //
 const {Client} = require('discord.js')
 const WS = require('./ws/ws')
 
