d28f4b2a2764f50eb387351fd394134c3f2ab4ef
Naomi Keller <thatstraveling@gmail.com>
2020-10-14 01:11:33 -0400

    im finna snap boi

--- src/bot/providers/ServerProvider.js
+++
@@ -1,5 +0,0 @@
-const Server = require('../classes/Server');
-
-module.exports = (container) => {
-  container.service('Server', c => new Server(c.Database));
-};
--- src/bot/providers/StatProvider.js
+++
@@ -1,5 +0,0 @@
-const Stat = require('../classes/Stat');
-
-module.exports = (container) => {
-  container.service('Stat', c => new Stat(c.Database));
-};
