b1e0ff0db863497286e298f16e0e34833f2d332f
U-DESKTOP-4TFK4I1\XeroXen <jlewis170@ivytech.edu>
2020-11-17 23:04:18 -0500

    Testing event creation

--- bot.js
+++ bot.js
@@ -57,6 +57,40 @@ client.on("message", async message => {
         });
         pool.end();  
     }
+
+    //testing below...
+    
+    if (cmd === `${prefix}cTestDatabase`){
+        pool.connect();
+
+        pool.query('CREATE TABLE TEST_EVENT (EVENT_ID SERIAL NOT NULL PRIMARY KEY, EVENT_NAME VARCHAR(100) NOT NULL); CREATE TABLE TEST_USER (USER_TAG TEXT PRIMARY KEY); CREATE TABLE TEST_SUBSCRIPTION (EVENT_ID INTEGER REFERENCES TEST_EVENT(EVENT_ID), USER_TAG TEXT REFERENCES TEST_USER(USER_TAG), PRIMARY KEY(EVENT_ID, USER_TAG));', (err, res) => {
+            //does this work?
+            if(err) throw err;
+            else{
+                message.channel.send("Test Tables created");
+            }
+        });
+    }
+
+    if (cmd === `${prefix}CreateEvent`){
+        let name = args[0];
+
+        pool.connect();
+        pool.query(`INSERT INTO TEST_EVENT VALUES (DEFAULT, ${name});`, (err, res) => {
+            if(err) throw err;
+            else{
+                message.channel.send("Event created with values: ");
+                pool.query(`SELECT * FROM TEST_EVENT WHERE EVENT_NAME = \'${name}\'`, (err, res) => {
+                    if(err) throw err;
+                    else{
+                        for(let row of res.rows){
+                            message.channel.send(JSON.stringify(row));
+                        }
+                    }
+                });
+            }
+        });
+    }
    
 });
 
