0bdd6d6cbe3ead3b7ce8f3659c9bcbb0c0e73387
Jack <elephanta.jx@gmail.com>
2020-11-19 17:56:38 -0500

    file testing

--- database.js
+++ database.js
@@ -39,10 +39,10 @@ class Database
     {
         if (newEvent instanceof Event)
         {
-            let query = `INSERT INTO EVENT (event_name, event_description, event_start, event_end, event_url) 
+            let query = `INSERT INTO TEVENT (event_name, event_description, event_start, event_end, event_url) 
                       VALUES (${newEvent.name}, ${newEvent.description}, ${newEvent.startTime}, ${newEvent.endTime}, ${newEvent.url});`;
             
-            
+            console.log(query);
             this.pool.query(query, (err, res) => {
                 if(err) 
                     throw err;
