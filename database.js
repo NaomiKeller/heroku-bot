2e68990e305b4c1eaf62bb2b3daa1186cf908f57
Jack <elephanta.jx@gmail.com>
2020-11-19 15:12:34 -0500

    file testing

--- database.js
+++ database.js
@@ -1,35 +1,40 @@
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Database file test version
 
-
-
-// create a event in the database
-// prcondition: an Event object 
-// postcondition: true for success and false for error
-function createEvent (newEvent) 
+class Database
 {
-    if (newEvent instanceof Event)
+    constructor(databaseHandle)
     {
-        let query = `INSERT INTO event (event_name, event_description, event_start, event_end, event_url)
-                    VALUES (${newEvent.name}, ${newEvent.description}, ${newEvent.startTime}, ${newEvent.endTime}, ${newEvent.name});`;
-            
-        databaseHandle.query(query, (err, res) => {
-                if(err) throw err;
-        });
-
-            
-        return true;
+        this.databaseHandle = databaseHandle;
     }
-    else 
-        return false;
+
+    // create a event in the database
+    // prcondition: an Event object 
+    // postcondition: true for success and false for error
+    createEvent (newEvent) 
+    {
+        if (newEvent instanceof Event)
+        {
+            let query = `INSERT INTO event (event_name, event_description, event_start, event_end, event_url)
+                        VALUES (${newEvent.name}, ${newEvent.description}, ${newEvent.startTime}, ${newEvent.endTime}, ${newEvent.name});`;
+            
+            databaseHandle.query(query, (err, res) => {
+                    if(err) throw err;
+            });
+
+            
+            return true;
+        }
+        else 
+            return false;
+    }
+
+    test()
+    {
+        return "file";
+    }
 }
 
-function test()
-{
-    return "file";
-}
-
-module.export.createEvent = createEvent;
-module.export.test = test;
+module.export.d = Database;
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
