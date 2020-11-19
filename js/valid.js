484b69307c9e43fb699464f09941b9a1a641e9e7
Naomi Keller <thatstraveling@gmail.com>
2020-11-18 21:39:15 -0500

    added login page. user is 'admin', password is 'pass' for now

---
+++ js/valid.js
@@ -0,0 +1,53 @@
+// Validation for login input
+var username = document.forms['form']['username']
+var password = document.forms['form']['password']
+
+var loginError = document.getElementById('loginError');
+
+username.addEventListener('click', reset);
+
+function validated() {
+    if (username.value === "admin" && password.value === "pass") {
+        return true;
+    } else {
+        username.style.border = "1px solid red";
+        password.style.border = "1px solid red";
+        loginError.style.display = "block";
+        username.focus();
+        password.focus();
+        return false;
+    }
+}
+
+function reset() {
+    loginError.style.display = "none";
+    username.style.border = "none";
+    password.style.border = "none";
+}
+/*
+function userVerify() {
+    if (username === "administrator") {
+        userError.style.display = "none";
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+function userVerify() {
+    if (username.value.length >= 8) {
+        userError.style.display = "none";
+        if (username.value === 'administrator') {
+            return true;
+        }
+    }
+}
+
+function passVerify() {
+    if (password.value.length >= 5) {
+        passError.style.display = "none";
+        return true;
+    }
+}
+*/
