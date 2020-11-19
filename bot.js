3aadc8147607f9bd4cedc30c5c8c5584fe1f01f7
Jack <elephanta.jx@gmail.com>
2020-11-19 15:27:00 -0500

    file testing

--- bot.js
+++ bot.js
@@ -8,43 +8,11 @@ const pool = new Pool({
     }
 });
 
-class Database
-{
-    constructor(databaseHandle)
-    {
-        this.databaseHandle = databaseHandle;
-    }
 
-    // create a event in the database
-    // prcondition: an Event object 
-    // postcondition: true for success and false for error
-    createEvent (newEvent) 
-    {
-        if (newEvent instanceof Event)
-        {
-            let query = `INSERT INTO event (event_name, event_description, event_start, event_end, event_url)
-                        VALUES (${newEvent.name}, ${newEvent.description}, ${newEvent.startTime}, ${newEvent.endTime}, ${newEvent.name});`;
-            
-            databaseHandle.query(query, (err, res) => {
-                    if(err) throw err;
-            });
-
-            
-            return true;
-        }
-        else 
-            return false;
-    }
-
-    test()
-    {
-        return "file";
-    }
-}
-//const Database = require('./database.js');
+const Database = require('./database.js');
 
 
-const mydatabase = new Database(pool);
+//const mydatabase = new Database(pool);
 
 class Event
 {
@@ -99,7 +67,7 @@ client.on("message", async message => {
     }
 
     if (cmd === `${prefix}file`) {
-        return message.channel.send(`${mydatabase.test()}`);
+      //  return message.channel.send(`${mydatabase.test()}`);
     }
     //testing database features below...
 
