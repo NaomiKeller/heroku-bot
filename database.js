eab89f7cebc0c133577061cd2cdeacb0d5ad1816
Jack <elephanta.jx@gmail.com>
2020-11-20 17:56:43 -0500

    command interface

--- database.js
+++ database.js
@@ -166,7 +166,7 @@ class Database
         {
             
             let query = `INSERT INTO Advertisement (advert_messageid, advert_eventid, advert_serverid) 
-                      VALUES (\'${newAdvert.messageId}\', ${newAdvert.eventId}, ${newAdvert.serverId} );`;
+                      VALUES (${newAdvert.messageId}, ${newAdvert.eventId}, ${newAdvert.serverId} );`;
             
             console.log(query);
             this.pool.query(query, (err, res) => {
