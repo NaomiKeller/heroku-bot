1d4ac8cc862cdef2651766329312b6e8cbf0bc3c
Naomi Keller <thatstraveling@gmail.com>
2020-11-23 18:43:38 -0500

    debugging

--- cal.js
+++ cal.js
@@ -1,13 +1,23 @@
-const express = require('express')
-const app = express()
-
-
-
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
 const PORT = process.env.PORT || 5000;
 
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+app.use('/', router);
+
+
+
+
+/*
 app.get('/', (req, res) => {
-  res.render('cal.html')
+  res.senfFile('cal.html')
 })
+*/
 
 app.listen(PORT, () => {
   console.log(`${PORT}`)
