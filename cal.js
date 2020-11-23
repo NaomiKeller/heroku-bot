5d87e64a002a90d581d856b74b0539ded2ad5e28
Naomi Keller <thatstraveling@gmail.com>
2020-11-23 18:02:09 -0500

    please god

--- cal.js
+++ cal.js
@@ -1,7 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3000
 
-const express = require('express');
-const app = new express();
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-app.get('/', function(request, response){
-    response.sendFile('cal.html');
-});
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
