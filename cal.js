b7960b4d87926725d152bd2d68fa3f3d9dfbd542
Jack <elephanta.jx@gmail.com>
2020-11-23 19:26:19 -0500

    web try 1

--- cal.js
+++ cal.js
@@ -4,6 +4,8 @@ const path = require('path');
 const router = express.Router();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.static('/'));
+
 router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname+'/index.html'));
 });
