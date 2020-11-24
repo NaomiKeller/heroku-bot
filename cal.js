f95a0fcaef54a984b829dc400ce93d6d31d798d1
Naomi Keller <thatstraveling@gmail.com>
2020-11-23 20:17:58 -0500

    a

--- cal.js
+++ cal.js
@@ -12,12 +12,35 @@ router.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname+'/index.html'));
 });
 
+/*
 app.get("/", (req, res) => {
   res.sendFile(`${__dirname}/home.html`);
 });
+*/
+//////////////////////////////////////////////////
+var username = document.forms['form']['username']
+var password = document.forms['form']['password']
 
+var loginError = document.getElementById('loginError');
 
+username.addEventListener('click', reset);
 
+function validated() {
+    if (username.value === "admin" && password.value === "pass") {
+        res.redirect('home.html');
+        return true;
+    } else {
+        loginError.style.display = "block";
+        username.focus();
+        password.focus();
+        return false;
+    }
+}
+
+function reset() {
+    loginError.style.display = "none";
+}
+////////////////////////////////////////////////
 //app.use('/', router);
 
 
