76a973f7e1fc5f90d5a8a5bd88aed17c4c7cffaf
Naomi Keller <thatstraveling@gmail.com>
2020-11-25 01:52:09 -0500

    is this semester over yet

--- cal.js
+++ cal.js
@@ -2,16 +2,28 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const router = express.Router();
+const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 5000;
 
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(express.static(__dirname));
 console.log(__dirname);
 
 router.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname+'/index.html'));
+	res.sendFile(path.join(__dirname+'/public/index.html'));
 });
+
+app.get('/home', (req, res) => {
+  res.sendFile(path.join(__dirname+'/public/home.html'));
+});
+
+app.post('/home', (req, res) => {
+  res.redirect("/home");
+});
+
 
 app.listen(PORT, () => {
   console.log(`${PORT}`)
-})	
+});	
+
