663e2b02f3b8a68b5b7fedb0bebdf5d0564e8e4a
Jack <elephanta.jx@gmail.com>
2020-12-05 16:02:07 -0500

    fix self ping

--- selfPing.js
+++ selfPing.js
@@ -7,18 +7,6 @@ const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 const request = new XMLHttpRequest();
 
-request.open("GET", "https://forgetmebot.herokuapp.com/");
-request.responseType = 'text';
-request.setRequestHeader('Content-type', 'application/json');
-    
-request.onload = function() {
-        
-    console.log(request);
-    
-};
-
-
-
 
 // selfping (http accessing) function
 async function selfPing()
@@ -26,6 +14,8 @@ async function selfPing()
 	while (1)
 	{
 		await timer(20*60*1000);		// check once every 20 minutes
+		request.open("GET", "https://forgetmebot.herokuapp.com/");
+
 		request.send();
 
 	}
