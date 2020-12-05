fd2b389e464d48e710936c7f8a9a050c6b18af18
Jack <elephanta.jx@gmail.com>
2020-12-05 15:06:49 -0500

    self ping (I think its working now)

--- selfPing.js
+++ selfPing.js
@@ -1,8 +1,11 @@
+// function timer
+const timer = ms => new Promise(res => setTimeout(res, ms));
 
 
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+// set up http access
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-var request = new XMLHttpRequest();
+const request = new XMLHttpRequest();
 
 request.open("GET", "https://forgetmebot.herokuapp.com/");
 request.responseType = 'text';
@@ -11,8 +14,24 @@ request.setRequestHeader('Content-type', 'application/json');
 request.onload = function() {
         
     console.log(request);
-    console.log(request.response);
+    
 };
 
-request.send();
+
+
+
+// selfping (http accessing) function
+async function selfPing()
+{
+	while (1)
+	{
+		await timer(20*60*1000);		// check once every 20 minutes
+		request.send();
+
+	}
+
+}
+
+selfPing();
+
 
