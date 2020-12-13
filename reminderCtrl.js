bd060987b72956645fcda9b09c3d63127be12371
Jack <elephanta.jx@gmail.com>
2020-12-13 15:41:52 -0500

    reminder ctrl read discord channel

--- reminderCtrl.js
+++ reminderCtrl.js
@@ -56,14 +56,15 @@ async function remControl()
 			let guild = await client.guilds.fetch(tempEvent.serverId);
 			//console.log(guild);
 
-
-			let system = await guild.channels.cache.get(`${guild.systemChannelID}`);
-			let channel = await client.channels.cache.get('753350045138550937');
-			let channel1 = await guild.channels.cache.get('753350045138550937');		//753350045138550937 //753350045138550937
-			console.log("system", system);
+			let channel = await guild.systemChannel;
 			console.log(channel);
-			console.log("channel from guild: ", channel1);
-			//channel.send("@Jack X");
+			//let system = await guild.channels.cache.get(`${guild.systemChannelID}`);
+			//let channel = await client.channels.cache.get('753350045138550937');
+			//let channel1 = await guild.channels.cache.get('753350045138550937');		
+			//console.log("system", system);
+			//console.log(channel);
+			//console.log("channel from guild: ", channel1);
+			channel.send("@Jack X");
 
 			if (deltaTime < 1000 * 60 && deltaTime > 0)
 			{
