7bcf02fc83a698cb320eb6b480807a97d92d5377
Jack <elephanta.jx@gmail.com>
2020-12-17 15:41:34 -0500

    visual cal

--- public/js/event.js
+++ public/js/event.js
@@ -166,3 +166,155 @@ function deleteEvent(form)
 
     return false;   
 }
+
+function displayCal()
+{
+    let request = new XMLHttpRequest();
+    let eventArray = [];
+
+    request.open("POST", "/cal");
+    request.responseType = 'text';
+    request.setRequestHeader('Content-type', 'application/json');
+    
+    request.onload = function() {
+        
+        if (request.response === 'empty')
+        {
+            alert("There are no events");
+        }
+        else 
+        {
+            eventArray = JSON.parse(request.response);
+
+            let html = "<table border='1|1'>";
+
+            const date = new Date(); 
+            const days = getDaysInMonth(date.getYear(), date.getMonth());
+            date.setDate(1);
+            const firstWeekDay = date.getDay();
+            const weeks = Math.ceil((days + firstWeekDay) / 7);
+
+            console.log(eventArray);
+
+
+            let filtered = eventArray.filter(event => 
+            {
+                let start = new Date(event.startTime);
+                console.log(start);
+                if (start.getMonth() === date.getMonth() && start.getYear() === date.getYear())
+                    return true;
+                else 
+                    return false;
+            });
+
+            console.log(filtered);
+
+            // headers
+            html += "<tr>";
+            html += "<th style=\"width:14%\"> Sun</th>";
+            html += "<th style=\"width:14%\">Mon</th>"; 
+            html += "<th style=\"width:14%\">Tue</th>"; 
+            html += "<th style=\"width:14%\">Wed</th>"; 
+            html += "<th style=\"width:14%\">Thu</th>"; 
+            html += "<th style=\"width:14%\">Fri</th>";  
+            html += "<th style=\"width:14%\">Sat</th>";  
+            html += "</tr>";
+
+    
+            let eventString;
+            let curDate = 0;
+            for (curWeek = 0; curWeek < weeks; curWeek++)
+            {
+                
+                html += "<tr style = \"line-height:80px\">";        // row starts
+
+                for (let i = 0; i < 7; i++)
+                {
+                    if ((curDate < 1 && i < firstWeekDay) || curDate >= days)
+                        html += "<td> </td>";
+                    else 
+                    {
+                        curDate++;
+                        html += "<td style=\"text-align:left;vertical-align:top\">" + curDate + "<br/>";
+
+                        for (let event of filtered)
+                        {
+                            checkStart = new Date(event.startTime);
+                            if (checkStart !== 0 && checkStart.getDate() === curDate)
+                            {
+                                let start = checkStart.getHours() + ":" + (checkStart.getMinutes() === 0 ? "00" : checkStart.getMinutes());
+                        
+                                eventString = "";
+                                eventString += (event.name + " - " + start);
+                                html += eventString + "</td>";
+                            }
+                        }
+
+                        html += "</td>";   
+                    }
+                }
+            
+                html += "</tr>";    // row ends
+      
+            }
+
+            document.getElementById("jsTable").innerHTML = html;
+        }
+        
+    };
+
+    request.send(JSON.stringify({
+        msg: "listEvent"
+    }));
+
+    
+}
+
+function getDaysInMonth(year, month)
+{
+    if (!Number.isInteger(year) || !Number.isInteger(month))
+        return undefined;
+
+    switch(month)
+    {
+        case 0:
+            return 31;
+        case 1:     // Feburary
+            if (year % 100 === 0)
+            {
+                if (year % 4 === 0)
+                    return 29;
+                else 
+                    return 28;
+            }
+            else if (year % 4 === 0)
+                return 29;
+            else 
+                return 28;
+
+        case 2:
+            return 31;
+        case 3:
+            return 30;
+        case 4:
+            return 31;
+        case 5:
+            return 30;
+        case 6:
+            return 31;
+        case 7:
+            return 31;
+        case 8:
+            return 30;
+        case 9:
+            return 31;
+        case 10:
+            return 30;
+        case 11:
+            return 31;
+        default:
+            return undefined;
+    
+    }
+
+}
